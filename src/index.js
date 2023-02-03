33709ecb687ba5b51eb363702c44b7255febc0b4
Peter Lee <PeterLee2094@gmail.com>
2023-02-03 14:38:04 -0600

    imported dependency

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+// saga 
+import createSagaMiddleware from '@redux-saga/core';
+import {takeEvery, put} from 'redux-saga/effects'
+
+//axios
+import axios from 'axios';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
