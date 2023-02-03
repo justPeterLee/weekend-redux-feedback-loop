500cf215495b3144c01b2e46f5a5132430008e98
Peter Lee <PeterLee2094@gmail.com>
2023-02-03 16:37:52 -0600

    added feedback reducer

--- src/index.js
+++ src/index.js
@@ -1,43 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 // redux
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-// saga 
-import createSagaMiddleware from '@redux-saga/core';
-import {takeEvery, put} from 'redux-saga/effects'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+// saga
+import createSagaMiddleware from "@redux-saga/core";
+import { takeEvery, put } from "redux-saga/effects";
 
 //axios
-import axios from 'axios';
-
-
+import axios from "axios";
 
 //----- saga ------
 const sagaMiddleware = createSagaMiddleware();
 
-// saga generator function 
-function* watcherSaga(){
+// saga generator function
+function* watcherSaga() {}
 
-}
+
 
 
 // ----- redux -----
+// # reducers
+// question answers
+function feedbackAnswer(state = {}, action) {
+  switch (action.type) {
+    case "ADD_FEELING":
+      return (state = { ...state, feeling: action.payload });
+    case "ADD_UNDERSTAND":
+      return (state = { ...state, understand: action.payload });
+    case "ADD_SUPPORT":
+      return (state = { ...state, support: action.payload });
+    case "ADD_COMMENT":
+      return (state = { ...state, comment: action.payload });
+    default:
+      return state;
+  }
+}
 
-// store instance 
+
+
+// store instance
 const store = createStore(
-    combineReducers({
-        // reducers 
-    }),
+  combineReducers({
+    // reducers
+    feedbackAnswer
+  }),
 
-    applyMiddleware(logger,sagaMiddleware)
-)
+  applyMiddleware(logger, sagaMiddleware)
+);
 
-sagaMiddleware.run(watcherSaga)
+sagaMiddleware.run(watcherSaga);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
